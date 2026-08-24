23bc518175ef8f718e466c1259a225ba4a23320f
shabarithasan <shabarithasan007@gmail.com>
2026-08-24 19:10:33 +0530

    Fix CORS for production frontend

--- backend/src/main/java/com/evalorithm/config/CorsConfig.java
+++ backend/src/main/java/com/evalorithm/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://evalorithm.web.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
